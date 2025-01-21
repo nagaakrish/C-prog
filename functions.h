097914856ec00f36cd60ed404e0581f21ff97fd6
nagaakrish <163498386+nagaakrish@users.noreply.github.com>
2025-01-21 20:57:20 +0545

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,8 @@
+// #ifndef FUNCTIONS_H
+// #define FUNCTIONS_H
+
+void addMember(const char *filename);
+void displayMembers(const char *filename);
+void deleteMember(const char *filename);
+
+// #endif 
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "functions.h" // Include the header file for function prototypes
+
+int main() {
+    const char *filename = "Ajima_Physical_Fitness_Members.txt"; 
+    int choice;
+
+    do {
+        printf("\n Ajima Physical Fitness Membership Management \n");
+        printf("1. Add a new payment record\n");
+        printf("2. Display all payment records\n");
+        printf("3. Delete a payment record\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addMember(filename);
+                break;
+            case 2:
+                displayMembers(filename);
+                break;
+            case 3:
+                deleteMember(filename);
+                break;
+            case 4:
+                printf("Exiting program. Goodbye!\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
