7186f92634d6356720be0424ac3ab181535baac0
nagaakrish <163498386+nagaakrish@users.noreply.github.com>
2025-01-21 20:59:11 +0545

    Delete functions.h

--- functions.h
+++
@@ -1,8 +0,0 @@
-// #ifndef FUNCTIONS_H
-// #define FUNCTIONS_H
-
-void addMember(const char *filename);
-void displayMembers(const char *filename);
-void deleteMember(const char *filename);
-
-// #endif 
